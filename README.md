f2160bafe8e66f593281c0b592e2484b37256ced
mbusomaphalala219-crypto <mbusomaphalala219@gmail.com>
2026-06-20 20:33:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-zhrhup6p
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/mbusomaphalala219-crypto/react-zhrhup6p)
---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default function App() {
+  return (
+      <div
+            style={{
+                    minHeight: "100vh",
+                            background: "#0A0A0A",
+                                    color: "white",
+                                            fontFamily: "sans-serif",
+                                                    padding: "40px 20px",
+                                                          }}
+                                                              >
+                                                                    <div
+                                                                            style={{
+                                                                                      maxWidth: "600px",
+                                                                                                margin: "0 auto",
+                                                                                                          textAlign: "center",
+                                                                                                                  }}
+                                                                                                                        >
+                                                                                                                                <h1 style={{ fontSize: "60px" }}>LIKE ❤️</h1>
+
+                                                                                                                                        <p style={{ fontSize: "24px" }}>
+                                                                                                                                                  No words. Just love.
+                                                                                                                                                          </p>
+
+                                                                                                                                                                  <p
+                                                                                                                                                                            style={{
+                                                                                                                                                                                        opacity: 0.7,
+                                                                                                                                                                                                    lineHeight: 1.6,
+                                                                                                                                                                                                                marginTop: "30px",
+                                                                                                                                                                                                                          }}
+                                                                                                                                                                                                                                  >
+                                                                                                                                                                                                                                            A social network where the only interaction is a like.
+                                                                                                                                                                                                                                                      No comments. No followers. No dislikes.
+                                                                                                                                                                                                                                                                Every post competes on a leaderboard that resets every 24 hours.
+                                                                                                                                                                                                                                                                        </p>
+
+                                                                                                                                                                                                                                                                                <button
+                                                                                                                                                                                                                                                                                          style={{
+                                                                                                                                                                                                                                                                                                      marginTop: "30px",
+                                                                                                                                                                                                                                                                                                                  padding: "15px 30px",
+                                                                                                                                                                                                                                                                                                                              borderRadius: "50px",
+                                                                                                                                                                                                                                                                                                                                          border: "none",
+                                                                                                                                                                                                                                                                                                                                                      fontSize: "18px",
+                                                                                                                                                                                                                                                                                                                                                                  cursor: "pointer",
+                                                                                                                                                                                                                                                                                                                                                                            }}
+                                                                                                                                                                                                                                                                                                                                                                                    >
+                                                                                                                                                                                                                                                                                                                                                                                              Join Waitlist
+                                                                                                                                                                                                                                                                                                                                                                                                      </button>
+                                                                                                                                                                                                                                                                                                                                                                                                            </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                  );
+                                                                                                                                                                                                                                                                                                                                                                                                                  }
